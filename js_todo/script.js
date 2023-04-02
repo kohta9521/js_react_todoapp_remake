edcdaebdeae35e9fc9c539a53f8abf40ed6fde0f
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-02 22:42:14 +0900

    add delete and complete button

--- js_todo/script.js
+++ js_todo/script.js
@@ -12,8 +12,24 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // button(完了)タグ作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("sample");
+  });
+
+  // button(削除)タグ作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("sample2");
+  });
+
   // divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // 未完了のリストに追加する
   document.getElementById("incomplete-list").appendChild(div);
