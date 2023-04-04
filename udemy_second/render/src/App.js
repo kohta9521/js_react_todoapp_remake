5c55228885d1732578fbc9db2e0a4cc565ab235a
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-04 19:32:23 +0900

    add css and app js coutup state

--- udemy_second/render/src/App.js
+++ udemy_second/render/src/App.js
@@ -3,9 +3,13 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const countUp = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
       <p>{count}</p>
+      <button onClick={countUp}>Count Up</button>
     </div>
   );
 }
