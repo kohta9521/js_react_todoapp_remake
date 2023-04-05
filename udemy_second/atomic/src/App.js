97f613027d497b719616aef898c369a200e17078
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-05 10:19:09 +0900

    add eorror about card1 not found but not found compiler error

--- udemy_second/atomic/src/App.js
+++ udemy_second/atomic/src/App.js
@@ -3,13 +3,24 @@ import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
 
+const user = {
+  name: "kohta",
+  image: "https://picsum.photos/200/200",
+  email: "kohta0521.gmail.com",
+  phone: "090-0000-0000",
+  company: {
+    name: "test com",
+  },
+  website: "https://google.com",
+};
+
 function App() {
   return (
     <div className="App">
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
-      <UserCard />
+      <UserCard user={user} />
     </div>
   );
 }
