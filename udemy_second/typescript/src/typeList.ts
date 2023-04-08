f08db734fef79a5d0e7a00a69dcb30a64daf5f26
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-08 15:06:42 +0900

    add type lost

---
+++ udemy_second/typescript/src/typeList.ts
@@ -0,0 +1,24 @@
+// typescriptの基本の型
+
+import { isUndefined } from "util";
+
+// boolean
+// var bool: boolean  = true;
+
+let number: number = 1;
+
+let arr1: Array<number> = [0, 1, 2];
+
+
+const func1 = (): void => {
+  const test = "test";
+}
+
+
+// null 
+let null1: null = null;
+
+// undefined
+
+// object
+let obj1: object = {};
