7ab7d690551c072d8a51e3fc3cdea04c635ede0f
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-08 17:28:12 +0900

    add Omit and Pick typsript props choice

--- udemy_second/typescript2/src/Todo.tsx
+++ udemy_second/typescript2/src/Todo.tsx
@@ -1,7 +1,13 @@
+import { TodoType } from "./types/todo";
 
-export const Todo =(props) => {
-  const { title, userId } = props;
+export const Todo =(
+  // props: Pick<TodoType, "userId" | "title" | "completed">
+    props: Omit<TodoType, "Id">
+  ) => {
+  const { title, userId, completed = false } = props;
+  const completMark = completed ? "[完]" : "[未]";
+
   return (
-    <p>{`${title}ユーザー:${userId}`}</p>
+    <p>{`${completMark} ${title}ユーザー:${userId}`}</p>
   )
 }
