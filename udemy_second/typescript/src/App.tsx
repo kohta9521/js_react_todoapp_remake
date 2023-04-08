011281576f2eaff50935ac80af82e31e0259ef2a
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-08 15:30:50 +0900

    add all files

--- udemy_second/typescript/src/App.tsx
+++ udemy_second/typescript/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Practice1 } from './practices/Practice1';
 import { Practice2 } from './practices/Practice2';
+import { Practice3 } from './practices/Practice3';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       hello 
       <Practice1 />
       <Practice2 />
+      <Practice3 />
     </div>
   );
 }
