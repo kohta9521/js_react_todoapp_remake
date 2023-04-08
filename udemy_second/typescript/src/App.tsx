434952899f9a7f0c0e91abdbd2813fd468a88ba2
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-08 15:25:17 +0900

    add 返却値の型指定

--- udemy_second/typescript/src/App.tsx
+++ udemy_second/typescript/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Practice1 } from './practices/Practice1';
+import { Practice2 } from './practices/Practice2';
 
 function App() {
   return (
     <div className="App">
       hello 
+      <Practice1 />
+      <Practice2 />
     </div>
   );
 }
